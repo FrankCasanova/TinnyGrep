ea167c38efd534a2568d5d0afed391cbee4ffad8
Frank Casanova <frankcasanova.info@gmail.com>
2025-01-20 14:29:22 +0100

    Refactor project structure: Move Config and run logic to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() != 3 {
-            panic!("please, enter just 2 arguments, the word you want find and the file path");
-        }
-        let query = args[1].clone();
-        let file_path: String = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text: \n {contents}");
-
-    Ok(())
-}
+use command_line_software::{run, Config};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
